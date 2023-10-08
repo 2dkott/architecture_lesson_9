7ab473685fa73cb7b51dd4b4fbedaf569cb1883a
kot2d <kot2d@outlook.com>
2023-10-08 22:57:53 +0200

    added sqlite jdbc template

--- src/main/java/com/gb/lesson_9/sevices/persistence/AppConfiguration.java
+++ src/main/java/com/gb/lesson_9/sevices/persistence/AppConfiguration.java
@@ -1,16 +1,20 @@
 package com.gb.lesson_9.sevices.persistence;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfiguration {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     @Bean
     public TemperatureRepository getTemperatureRepository() {
-        return new TempTemperatureRepository();
+        return new TempTemperatureRepository(jdbcTemplate);
     }
 
 }
